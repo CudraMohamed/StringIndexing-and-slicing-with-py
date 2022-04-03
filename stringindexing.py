3fe7dadf66214def97d63a3371e50c03029a383f
CudraMohamed <ncudramohamed@gmail.com>
2022-04-03 18:22:32 +0300

    indexing and slicing changes

--- stringindexing.py
+++ stringindexing.py
@@ -14,3 +14,22 @@ print(school[8])
 # String negative starts with -1 form the left
 print(school[-8])
 print(school[-1])
+
+
+#Given the string p= "Hello Python world"
+#Conduct the following exercises
+#Using positive indexing to print the first chracter of each word in the string
+
+#0    1   2   3   4     5    6    7    8    9  10   11   12  13   14    15   16   17
+#H    e   l   l   o          P    y    t    h   o   n         W    o    r    l    d
+#-18 -17 -16 -15  -14  -13  -12  -11  -10   -9  -8  -7   -6   -5   -4   -3   -2    -1
+
+p="Hello Python World"
+print(p[0])
+print(p[6])
+print(p[13])
+
+#Use the negative indexing to print the last character of each word in the string
+print(p[-14])
+print(p[-7])
+print(p[-1])
--- stringslicing.py
+++ stringslicing.py
@@ -1,6 +1,7 @@
 #STRING SLICING
 #Accessing a range of character not just one through there index positions
 #You can command negative and positive indexing
+#In indexing and slicing spacing is also sounted as a character
 
 #Index from left to right even in negative numberhed by supplying 2 index positions separated by a colon e.g
 #School [1:4] #the last index position is not included in the output
@@ -12,3 +13,38 @@ print(school[1:4])
 # emits the last position
 print(school[-4:-1])
 
+#Given the string p= "Hello Python world"
+#Conduct the following exercises
+
+#0    1   2   3   4     5    6    7    8    9  10   11   12  13   14    15   16   17
+#H    e   l   l   o          P    y    t    h   o   n         W    o    r    l    d
+#-18 -17 -16 -15  -14  -13  -12  -11  -10   -9  -8  -7   -6   -5   -4   -3   -2    -1
+
+p="Hello Python World"
+#Use positive number slicing to print
+#Python
+#Hell
+#World
+print(p[6:12])
+print(p[0:4])
+print(p[13:])
+
+#Use the negative number slicing to print
+#Python World
+#Hello python
+
+print(p[-12:])
+print(p[-18:-6])
+
+#replacing letters
+#replace any occurence of o with a
+print(p.replace("o","a"))
+
+#convert the string to uppercase
+print(p.upper())
+
+#convert the string to lowercase
+print(p.lower())
+
+#find the number of occurence of character "o"
+print(p.count("o"))
